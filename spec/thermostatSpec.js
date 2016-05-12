59065bd45e76e2173e8d55af45cd86bd02d0ff1c
Mara Wanot <m.d.wanot@hotmail.com>
2016-05-12 10:56:38 +0100

    added down temp button, min and max temp, power saving mode and it's default

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -8,9 +8,50 @@ describe("Thermostat", function() {
     expect(thermostat.temperature).toEqual(20);
   });
 
+  it("starts in power saving mode", function() {
+    expect(thermostat.powerSaving).toEqual(true);
+  });
+
   it("Up temperature button", function() {
     thermostat.increase();
     expect(thermostat.temperature).toEqual(21);
+  });
+
+  it("Down temperature button", function() {
+    thermostat.decrease();
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  it("Minimum temperature is 10 degrees", function() {
+    thermostat.temperature = 10;
+    thermostat.decrease();
+    expect(thermostat.temperature).toEqual(10);
+  });
+
+  it("can turn power saving mode off", function() {
+    thermostat.powerSavingToggle();
+    expect(thermostat.powerSaving).toEqual(false);
+  });
+
+  it("can turn power saving mode on", function() {
+    thermostat.powerSaving = false;
+    thermostat.powerSavingToggle();
+    expect(thermostat.powerSaving).toEqual(true);
+  });
+
+  it("Max temperature with Power Saving Mode ON is 25 degrees", function() {
+    expect(thermostat.MAX_TEMP).toEqual(25);
+  });
+
+  it("Max temperature with Power Saving Mode OFF is 32 degrees", function() {
+    thermostat.powerSavingToggle();
+    expect(thermostat.MAX_TEMP).toEqual(32);
+  });
+
+  it("Can't increase past max temp", function() {
+    thermostat.temperature = thermostat.MAX_TEMP;
+    thermostat.increase();
+    expect(thermostat.temperature).toEqual(thermostat.MAX_TEMP);
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,8 +1,27 @@
 function Thermostat() {
   this.temperature = 20;
+  this.powerSaving = true;
+  this.MAX_TEMP = 25;
 }
 
 Thermostat.prototype.increase = function() {
-  this.temperature ++;
+  if (this.temperature < this.MAX_TEMP) {
+    this.temperature ++;
+  }
+};
 
+Thermostat.prototype.decrease = function() {
+  if (this.temperature > 10) {
+    this.temperature --;
+  }
+};
+
+Thermostat.prototype.powerSavingToggle = function() {
+  if (this.powerSaving === true) {
+    this.powerSaving = false;
+    this.MAX_TEMP = 32;
+  } else {
+    this.powerSaving = true;
+    this.MAX_TEMP = 25;
+  }
 };
